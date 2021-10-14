6b48accd508d99e533f725ef7a54fa14ff952e95
LilyWS <stranger1212ss23@gmail.com>
2021-10-14 16:36:11 -0400

    debugging heroku deploy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI, {// || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
